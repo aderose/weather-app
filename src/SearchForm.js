0fefe16332efe099cd0a931408c0bcf90ab1687f
aderose <sandro.derose28@yahoo.com>
2020-11-22 22:27:59 +0000

    Control search form state and elements

---
+++ src/SearchForm.js
@@ -0,0 +1,27 @@
+export default class SearchForm {
+  constructor({ formId, inputId, barId, errorId }) {
+    this.searchForm = document.querySelector(formId);
+    this.search = document.querySelector(inputId);
+    this.searchBar = document.querySelector(barId);
+    this.error = document.querySelector(errorId);
+  }
+
+  isValidInput() {
+    if (this.search.validity.valid && this.search.value.length > 0) {
+      this.searchBar.classList.remove('invalid');
+      this.error.textContent = '';
+      return true;
+    }
+    this.searchBar.classList.add('invalid');
+    this.error.textContent = 'Please provide a city name only.';
+    return false;
+  }
+
+  listen() {
+    this.searchForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      if (!this.isValidInput()) return;
+      console.log(this.search.value);
+    });
+  }
+}
