95387f0df3c5a39998af5639a809d655adccda8b
aderose <sandro.derose28@yahoo.com>
2020-11-22 19:37:24 +0000

    Hide test card

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import card from './card';
 
-document.querySelector('.content').appendChild(
-  card.create({
-    location: 'London, UK',
-    temperature: 30,
-    measurement: 'C',
-    imageCode: '09n',
-    description: 'Drizzling rain.',
-    humidity: 100,
-    windSpeed: 30,
-  })
-);
+window.addEventListener('load', () => {
+  const content = document.querySelector('.content');
+
+  // create and display a weather card with the provided details
+  function displayCard(details) {
+    content.innerHTML = '';
+    content.appendChild(card.create(details));
+  }
+
+  // displayCard({
+  //   location: 'London, UK',
+  //   temperature: 30,
+  //   measurement: 'C',
+  //   imageCode: '09n',
+  //   description: 'Drizzling rain.',
+  //   humidity: 100,
+  //   windSpeed: 30,
+  // });
+});
