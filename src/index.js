eb38cd204464856d83447244659eab772fa4f16c
aderose <sandro.derose28@yahoo.com>
2020-11-22 22:28:38 +0000

    Use SearchForm object to listen and validate search input

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import card from './card';
+import SearchForm from './SearchForm';
 
 window.addEventListener('load', () => {
-  const content = document.querySelector('.content');
+  const content = document.querySelector('#content');
+
+  // create a form object and listen
+  const searchForm = new SearchForm({
+    formId: '#search-form',
+    inputId: '#search',
+    barId: '#search-bar',
+    errorId: '#error',
+  });
+
+  searchForm.listen();
 
   // create and display a weather card with the provided details
   function displayCard(details) {
