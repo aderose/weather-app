d2f5bff6648e7d1611b0732e83005c29c6303b18
aderose <35845363+aderose@users.noreply.github.com>
2020-11-22 18:58:08 +0000

    Feature/card generator (#3)
    * Create card element factory
    
    * Add generated card to the main page content
    
    * Remove default card from HTML

---
+++ src/card.js
@@ -0,0 +1,62 @@
+export default (function card(doc) {
+  function generateHtml({
+    location,
+    temperature,
+    measurement,
+    imageCode,
+    description,
+    humidity,
+    windSpeed,
+  }) {
+    return `
+      <section class="card">
+      <h2>${location}</h2>
+      <div class="card-body">
+        <div class="summary">
+          <h3>${temperature}°${measurement}</h3>
+          <img
+            src="https://openweathermap.org/img/wn/${imageCode}@2x.png"
+            alt="Weather icon"
+          />
+        </div>
+        <div class="description">
+          <p>${description}</p>
+        </div>
+        <div class="extra-details">
+          <div class="humidity">
+            <i class="fas fa-tint"></i>
+            <p>${humidity}%</p>
+          </div>
+          <div class="wind">
+            <i class="fas fa-wind"></i>
+            <p>${windSpeed}mph</p>
+          </div>
+        </div>
+      </div>
+    </section>`;
+  }
+
+  function create({
+    location,
+    temperature,
+    measurement,
+    imageCode,
+    description,
+    humidity,
+    windSpeed,
+  }) {
+    const element = doc.createElement('section');
+    element.innerHTML = generateHtml({
+      location,
+      temperature,
+      measurement,
+      imageCode,
+      description,
+      humidity,
+      windSpeed,
+    });
+    return element;
+  }
+
+  return { create };
+})(document);
