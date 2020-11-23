db667ec01c23a3e27f33c44ac49a54f3a267bd6c
aderose <sandro.derose28@yahoo.com>
2020-11-23 17:18:27 +0000

    create card using search results

--- src/index.js
+++ src/index.js
@@ -1,25 +1,42 @@
 import card from './card';
 import SearchForm from './SearchForm';
 import pubsub from './pubsub';
+import config from './config';
 
 window.addEventListener('load', () => {
   const content = document.querySelector('#content');
 
   // create and display a weather card with the provided details
-  function displayCard(details) {
+  function displayCard(data) {
     content.innerHTML = '';
-    content.appendChild(card.create(details));
+    const weather = data.weather[0];
+    content.appendChild(
+      card.create({
+        location: `${data.name}, ${data.sys.country}`,
+        temperature: Math.round(data.main.temp - 273.15),
+        measurement: 'C',
+        imageCode: weather.icon,
+        description: weather.description,
+        humidity: data.main.humidity,
+        windSpeed: Math.round(data.wind.speed * 2.23694),
+      })
+    );
   }
 
-  // displayCard({
-  //   location: 'London, UK',
-  //   temperature: 30,
-  //   measurement: 'C',
-  //   imageCode: '09n',
-  //   description: 'Drizzling rain.',
-  //   humidity: 100,
-  //   windSpeed: 30,
-  // });
+  async function fetchData(search) {
+    return fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${config.WEATHER_API_KEY}`
+    ).then((response) => response.json());
+  }
+
+  async function processSearch(searchTerm) {
+    try {
+      const data = await fetchData(searchTerm);
+      displayCard(data);
+    } catch (err) {
+      console.log('City not found.');
+    }
+  }
 
   // create a form object and listen
   const searchForm = new SearchForm({
@@ -31,7 +48,5 @@ window.addEventListener('load', () => {
 
   searchForm.listen();
 
-  pubsub.subscribe('search', (search) => {
-    console.log(search);
-  });
+  pubsub.subscribe('search', processSearch);
 });
