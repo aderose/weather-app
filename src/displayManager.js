71dfe8b27d8d9ad4555276502e9907cd0b66b9e3
aderose <sandro.derose28@yahoo.com>
2020-11-25 19:46:16 +0000

    Update card temperature display when metric selection is changed

--- src/displayManager.js
+++ src/displayManager.js
@@ -14,19 +14,31 @@ export default (function displayManager() {
     const weather = data.weather[0];
     return card.create(undefined, {
       location: `${data.name}, ${data.sys.country}`,
+      // convert from kelvin to celcius
       temperature: Math.round(data.main.temp - 273.15),
       measurement: 'C',
       imageCode: weather.icon,
       description: weather.description,
       humidity: data.main.humidity,
+      // convert from m/s to mph
       windSpeed: Math.round(data.wind.speed * 2.23694),
     });
   }
 
-  // display the newly created card
+  // update the temperature display when the radio choice is changed
+  function listenForChange() {
+    document.querySelectorAll('input[name="metric"]').forEach((radio) => {
+      radio.addEventListener('change', (e) => {
+        card.updateTemperature(e.target.nextElementSibling.textContent);
+      });
+    });
+  }
+
+  // display the newly created card and listen for changes
   function displayCard(data) {
     display.innerHTML = '';
     display.appendChild(createCard(data));
+    listenForChange();
   }
 
   return { displayCard };
