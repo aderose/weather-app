34284cab7616bdd7b2fb16be8376ed40d5da5c6a
aderose <sandro.derose28@yahoo.com>
2020-11-25 15:45:23 +0000

    Add temperature measurement toggle to card

--- src/card.js
+++ src/card.js
@@ -9,10 +9,24 @@ export default (function card(doc) {
     windSpeed,
   }) {
     return `
-      <h2>${location}</h2>
+      <div class="card-header">
+        <h2>${location}</h2>
+        <div class="buttons">
+          <label class="button-container">
+            <input type="radio" name="metric" checked>
+            <span class="radio">°C</span>
+          </label>
+          <label class="button-container">
+            <input type="radio" name="metric">
+            <span class="radio">°F</span>
+          </label>
+        </div>
+      </div>
       <div class="card-body">
         <div class="summary">
-          <h3>${temperature}°${measurement}</h3>
+          <h3>
+            <span id="temperature">${temperature}</span>°<span id="metric">${measurement}</span>
+          </h3>
           <img
             src="https://openweathermap.org/img/wn/${imageCode}@2x.png"
             alt="Weather icon"
@@ -28,12 +42,16 @@ export default (function card(doc) {
             <i class="fas fa-wind"></i>
             <p>${windSpeed}mph</p>
           </div>
-        </div>`;
+        </div>
+      </div>
+    `;
   }
 
   function generateError({ error, message }) {
     return `
-      <h2>${error}</h2>
+      <div class="card-header">
+        <h2>${error}</h2>
+      </div>
       <div class="card-body error-message">
         ${message}
       </div>
@@ -43,10 +61,11 @@ export default (function card(doc) {
   function create(error, details) {
     const element = doc.createElement('section');
     element.classList.add('card');
-    element.innerHTML =
-      error === undefined
-        ? generateHtml(details)
-        : generateError({ error, message: details });
+    if (error === undefined) {
+      element.innerHTML = generateHtml(details);
+    } else {
+      element.innerHTML = generateError({ error, message: details });
+    }
     return element;
   }
 
